c6a4277d76e025db67361d122ac6672bd40a183d
JuanV <juanjosevivasrivera@gmail.com>
2020-07-17 11:34:16 -0500

    puse hola m

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -15,6 +15,8 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("holaaa");
         // TODO code application logic here
     }
     
